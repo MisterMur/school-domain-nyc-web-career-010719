26a4281c9128f7720e5382e11cf1a57ffdbd5041
Brian Murillo <brian.j.murillo@gmail.com>
2019-01-09 20:52:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,12 @@
 # code here!
 class School
   
+  attr_accessor :roster
+  
   def initialize(roster=Hash.new)
     @roster = Hash.new
   end
-  def roster
-    @roster
-  end
+  # def roster
+  #   @roster
+  # end
 end
